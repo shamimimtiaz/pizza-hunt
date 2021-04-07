81d7d15b4842125acc4b4ff9bf3a2c46713f97fc
Shamim Imtiaz <km_si@ymail.com>
2021-04-06 22:48:06 -0400

    create comment model, route amd controller

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v' //we also used the select option inside of populate(), so that we can tell Mongoose that we don't care about the __v field on comments either. The minus sign - in front of the field indicates that we don't want it to be returned
+      })
+      .select('-__v') //update the query to not include the pizza's __v
+      .sort({ _id: -1 }) //use .sort({ _id: -1 }) to sort in DESC order by the _id value
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -14,6 +20,11 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+       path: 'comments',
+       select:'-__v' 
+      })
+      .select('-__v')
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
